1539d0a0481c48cb81b2b417b23a6f4eeaa8a71a
gaurav876409 <gaurav876409@gmail.com>
2023-07-20 09:08:35 +0530

    up

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -9,33 +9,55 @@
     }
 */
 
-function grantAccessTo(roles) {
-    return (req, res, next) => {
-        try {
-            /*
-            Write your middleware here.
-            Steps: 
-            - Define a middleware function that takes the request, response, and next function as parameters.
-            - Extract the 'role' from req.body.
-            - Check if the required 'role' is present in the array.
-            - Return a valid response if access cannot be granted.
-            */
-           const {role} = req.body;
-           if(!roles.includes(role)){
-            return res.status(403).json({
-                status: "error",
-                message: "Access Denied",
-            });
-           }
-           next();
-        } catch (err) {
-            return res.status(400).json({
-                status: "error",
-                message: "Unable to check access level"
-            })
-        }
-    }
-}
+// function grantAccessTo(roles) {
+//     return (req, res, next) => {
+//         try {
+//             /*
+//             Write your middleware here.
+//             Steps: 
+//             - Define a middleware function that takes the request, response, and next function as parameters.
+//             - Extract the 'role' from req.body.
+//             - Check if the required 'role' is present in the array.
+//             - Return a valid response if access cannot be granted.
+//             */
+//            const {role} = req.body;
+//            if(!roles.includes(role)){
+//             return res.status(403).json({
+//                 status: "error",
+//                 message: "Access Denied",
+//             });
+//            }
+//            next();
+//         } catch (err) {
+//             return res.status(400).json({
+//                 status: "error",
+//                 message: "Unable to check access level"
+//             })
+//         }
+//     }
+// }
 
 // Export the middleware function as a module.
 module.exports = grantAccessTo;
+function grantAccessTo(roles) {
+    try {
+        return function (req, res, next) {
+            const { role } = req.body;
+            if (!roles.includes(role)) {
+                return res.status(403).json({
+                    status: "error",
+                    message: "Access Denied"
+                })
+            } else {
+                next();
+            }
+        }
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            message: "Unable to check access level"
+        })
+    }
+}
+
+module.exports = { grantAccessTo };
